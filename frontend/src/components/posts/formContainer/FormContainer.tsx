8b537061ee19118bd5b5cd1cc6742984983b2a03
SEMI-SAINT-GAMES <120226332+SEMI-SAINT-GAMES@users.noreply.github.com>
2025-04-13 20:55:47 +0300

    prepeared for host

--- frontend/src/components/posts/formContainer/FormContainer.tsx
+++ frontend/src/components/posts/formContainer/FormContainer.tsx
@@ -1,25 +1,16 @@
 import postsService from "../../../services/apiServices/posts/postsService.ts";
-import {useState } from "react";
-import { LoadingPage } from "../../../pages/loadingPage/LoadingPage.tsx";
 import './postFormStyles.scss'
 import PostForm from "./form/PostForm.tsx";
 
 
 const FormContainer = () => {
 
-    const [loading, setLoading] = useState(false);
 
 
     return (
-        <>
-            {loading ?
-                <LoadingPage />
-                :
-                <div className="Post-form">
-                    <PostForm setLoading={setLoading} service={postsService.createPost}/>
-                </div>
-            }
-        </>
+            <div className="Post-form">
+                <PostForm  service={postsService.createPost}/>
+            </div>
     );
 };
 
--- frontend/src/pages/loadingPage/LoadingPage.tsx
+++
@@ -1,8 +0,0 @@
-import './LoadingPageStyles.scss'
-export const LoadingPage: React.FC<{  }> = props => {
-    return (
-        <div className="loader-container">
-            <div className="loader"></div>
-        </div>
-    );
-};
